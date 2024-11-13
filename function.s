dc4050a16d48d5fe2f38c97767388ce2b1618162
mug4z <mug4z@protonmail.com>
2024-11-13 23:03:33 +0100

    Add function exemple

---
+++ function.s
@@ -0,0 +1,17 @@
+SECTION .data
+msg: db "Salut" ,6
+
+SECtION .text
+global _start
+_start:
+	call helloworld
+	mov rax, 60
+	syscall
+
+helloworld:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 5
+	syscall
+	ret
