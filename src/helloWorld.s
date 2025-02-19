86a9bcc8ac2a3a8fea904849f47f1667f4f167d3
mug4z <mug4z@protonmail.com>
2025-02-19 21:25:21 +0100

    Add exit code and newline character

--- src/helloWorld.s
+++ src/helloWorld.s
@@ -1,14 +1,23 @@
 SECTION .data
 msg: db "Salut" ,6 
+newline: db 10
 
 SECTION .text
-global main		
-main:
+
+global _start
+_start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
 	mov rdx, 5
 	syscall
 
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, newline
+	mov rdx, 1
+	syscall
+
 	mov rax, 60
+	mov rdi, 1
 	syscall
