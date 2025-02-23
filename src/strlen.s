a96949e71e782a2f462de60838785be787764f26
mug4z <mug4z@protonmail.com>
2025-02-23 15:34:18 +0100

    Change the srtlen.s
    Begin to write the strlen function in assembly.

--- src/strlen.s
+++ src/strlen.s
@@ -1,17 +1,7 @@
-SECTION .text 
+SECTION .data
+msg		db "Bonjour je suis pilote, il est l'heure", 0Ah
+
+SECTION .text
 global _start
-
-loop_start:
-	add rcx, 1
-	cmp rcx, 5
-	jne loop_start
-	jmp end
-end:
-	mov rax, 60
-	mov rdi, rcx
-	syscall
-
 _start:
-	mov rcx, 0
-	call loop_start
 
