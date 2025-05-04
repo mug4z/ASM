1c977f57e629fc8ad736dcf1d33026745fc7a91d
mug4z <mug4z@protonmail.com>
2025-05-04 17:00:13 +0200

    Try to use the call and ret operator

---
+++ src/subroutine.s
@@ -0,0 +1,29 @@
+SECTION .data
+msg:    db "Bonjour je suis pilote, il est l'heure", 10
+
+SECTION .text
+global  _start
+_start:
+	mov rbx, msg ; Move the address of the message in rbx
+	mov rdi, rbx ; Move the address in rbx into rdi, they now point to the same section in memory
+	call strlen  ; Call the subroutine (function) strlen
+	mov rdx,rdi
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	syscall
+
+	mov rax, 60
+	mov rdi, 1	
+	syscall
+
+strlen:
+    cmp byte [rdi], 0
+    jz  finished
+    inc rdi
+    jmp strlen
+
+finished:
+    sub rdi, rbx
+    ret
+
