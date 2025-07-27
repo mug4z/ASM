d30752440b16feea8304618a5b7c2585aafddd42
camille <camille@localhost.localdomain>
2025-07-27 16:31:10 +0200

    chore: add comment

--- src/userInput/userInput.s
+++ src/userInput/userInput.s
@@ -6,7 +6,8 @@ SECTION .bss
 ;variableName3:      RESD    1       ; reserve space for 1 double word
 ;variableName4:      RESQ    1       ; reserve space for 1 double precision float (quad word)
 ;variableName5:      REST    1       ; reserve space for 1 extended precision float
-userInput resb 255
+userInput resb 255 ;TODO: Check why if the userInput is overloaded the rest of the byte are given to the terminal as input.
+
 
 SECTION .data
 msg1 db 'Please enter your name: ',0
@@ -23,7 +24,7 @@ call print
 mov rax, 0
 mov rdi, 0
 mov rsi, userInput
-mov rdx, 255
+mov rdx, 255 
 syscall
 
 mov rax, msg2
@@ -34,3 +35,4 @@ call print
 
 call quit
 
+;NOTE: Interesting is that if more than 255 character are given in the userInputthe first 255 byte are read and the rest of them are given to the terminal
